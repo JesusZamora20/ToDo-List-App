3bc58c953f070fd965a68453b0df05023ae86c32
JesusZamora20 <jbermudezamora87@gmail.com>
2022-11-04 14:02:33 -0500

    saving tasks in localStorage

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 const taskInput = document.querySelector(".task-input input");
+
+taskInput.addEventListener("keyup", e => {
+    let userTask = taskInput.value.trim();
+    if(e.key == "Enter" && userTask){
+        //getting localstorage toDo list
+        let toDos = JSON.parse(localStorage.getItem("todo-list"));
+        if(!toDos){
+            toDos = [];
+        }
+        taskInput.value = "";
+        let taskInfo = {name: userTask, status: "pending"};
+        toDos.push(taskInfo);
+        localStorage.setItem("todo-list",JSON.stringify(toDos));
+        console.log(toDos);
+    }
+});
